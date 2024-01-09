a84ac07497b89f96eaf29012df720c30060158ec
moezabdelkef <moezbadelkefi17@gmail.com>
2024-01-09 14:11:26 +0100

    Basic application

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import HolbertonLogo from '../src/HolbertonLogo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={HolbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
