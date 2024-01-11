3db132618432c4dea5ce53da4dc4d2844e460703
moezabdelkef <moezbadelkefi17@gmail.com>
2024-01-11 13:16:17 +0100

    Create a project using Webpack

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../dist'),
+        },
+        hot: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['image-webpack-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+    devtool: 'inline-source-map',
+};
