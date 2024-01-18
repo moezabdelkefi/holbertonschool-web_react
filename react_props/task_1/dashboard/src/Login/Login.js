87ba0cbd2a1bd0eb8ba4a82582679f68b4548388
moezabdelkef <moezbadelkefi17@gmail.com>
2024-01-18 12:23:10 +0100

    Write the tests for each component

---
+++ react_props/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Login.css';
+import HolbertonLogo from '../assets/HolbertonLogo.jpg';
+import { getFooterCopy, getFullYear } from '../utils/utils';
+
+function Login() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={HolbertonLogo} alt="Holberton Logo" />
+                <h1>School dashboard</h1>
+            </div>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" />
+
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" />
+                    <button>OK</button>
+                </div>
+            </div>
+            <div className="App-footer">
+                Copyright {getFullYear()} - {getFooterCopy(true)}
+            </div>
+        </div>
+    );
+}
+
+export default Login;
