ed6c15a566b199c61926b1c7e742c00fe4c88f12
moezabdelkef <moezbadelkefi17@gmail.com>
2024-01-18 16:09:11 +0100

    Split the Notifications component

--- react_props/task_2/dashboard/src/Footer/Footer.js
+++ react_props/task_2/dashboard/src/Footer/Footer.js
@@ -1,29 +1,13 @@
-import React from 'react';
-import './Footer.css';
-import HolbertonLogo from '../assets/HolbertonLogo.jpg';
-import { getFooterCopy, getFullYear } from '../utils/utils';
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import "./Footer.css";
 
 function Footer() {
     return (
-        <div className="App">
-            <div className="App-header">
-                <img src={HolbertonLogo} alt="Holberton Logo" />
-                <h1>School dashboard</h1>
-            </div>
-            <div className="App-body">
-                <p>Login to access the full dashboard</p>
-                <div className="form-group">
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" />
-
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" />
-                    <button>OK</button>
-                </div>
-            </div>
-            <div className="App-footer">
+        <div className="footer">
+            <p>
                 Copyright {getFullYear()} - {getFooterCopy(true)}
-            </div>
+            </p>
         </div>
     );
 }
