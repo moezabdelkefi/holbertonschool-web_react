a6691a63dc776d2ad0b5f8409b35d758f1b0af14
moezabdelkef <moezbadelkefi17@gmail.com>
2024-01-27 15:13:23 +0100

    Use the new components

--- React_component/task_3/dashboard/src/App/App.js
+++ React_component/task_3/dashboard/src/App/App.js
@@ -5,6 +5,8 @@ import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
 import "./App.css";
 
 class App extends React.Component {
@@ -17,49 +19,40 @@ class App extends React.Component {
         { id: 3, name: 'React', credit: 40 },
       ]
     };
-    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown(event) {
-    if (event.ctrlKey && event.key === 'h') {
-      alert('logging you out');
-    }
-  }
-
   render() {
     return (
-      <>
+      <React.Fragment>
         <Notifications />
         <div className="App">
           <Header />
-        </div>
-        <div className="App-body">
-          {this.props.isLoggedIn ? <CourseList listCourses={this.state.listCourses} /> : <Login />}
-        </div>
-        <div className="App-footer">
+          <BodySectionWithMarginBottom title='Log in to continue'>
+            <Login />
+          </BodySectionWithMarginBottom>
+          <BodySectionWithMarginBottom title='Course list'>
+            <CourseList listCourses={this.state.listCourses} />
+          </BodySectionWithMarginBottom>
+          <BodySection title='News from the School'>
+            <p>Some random text</p>
+          </BodySection>
           <Footer />
         </div>
-      </>
+      </React.Fragment>
     );
   }
 }
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => { },
 };
 
 export default App;
