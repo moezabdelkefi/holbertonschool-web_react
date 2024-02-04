9dca41bf5c95f5b6b8f8c5b6e0dbf4d8898912b4
moezabdelkef <moezbadelkefi17@gmail.com>
2024-02-04 10:58:37 +0100

    Context

---
+++ react_state/task_2/dashboard/src/App/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+import { StyleSheet, css } from 'aphrodite';
+import AppContext from './AppContext';
+
+const styles = StyleSheet.create({
+  body: {
+    fontfamily: 'Arial, sans-serif',
+    margintop: 50,
+    marginleft: 30,
+  },
+  footer: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+    borderTop: '3px solid #e0344a',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    backgroundColor: '#fff',
+  },
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
+      ],
+      displayDrawer: false,
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email: email,
+        password: password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    this.setState({
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    });
+  }
+
+  render() {
+    const { displayDrawer, user } = this.state;
+
+    return (
+      <React.Fragment>
+        <AppContext.Provider value={{ user, logOut: this.logOut }}>
+          <div className={css(styles.body)}>
+            <Notifications
+              displayDrawer={displayDrawer}
+              handleDisplayDrawer={this.handleDisplayDrawer}
+              handleHideDrawer={this.handleHideDrawer}
+            />
+            <Header />
+            <BodySectionWithMarginBottom title='Log in to continue'>
+              {/* Pass the logIn function to the Login component */}
+              <Login logIn={this.logIn} />
+            </BodySectionWithMarginBottom>
+            <BodySectionWithMarginBottom title='Course list'>
+              <CourseList listCourses={this.state.listCourses} />
+            </BodySectionWithMarginBottom>
+            <BodySection title='News from the School'>
+              <p>Some random text</p>
+            </BodySection>
+            <Footer className={css(styles.footer)} />
+          </div>
+        </AppContext.Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  displayDrawer: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  displayDrawer: false,
+};
+
+export default App;
