0bc9d2f2bdcef265b9f760c90bdc7767c80883b9
moezabdelkef <moezbadelkefi17@gmail.com>
2024-02-28 10:50:16 +0100

    Create the reducer for notifications

---
+++ react_redux_reducer_selector/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,53 @@
+// NotificationItem.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    default: {
+        color: 'blue',
+    },
+    urgent: {
+        color: 'red',
+    },
+    item: {
+        '@media (max-width: 900px)': {
+            width: '100%',
+            borderBottom: '1px solid black',
+            fontSize: '20px',
+            padding: '10px 8px',
+        },
+    },
+});
+
+class NotificationItem extends React.PureComponent {
+    render() {
+        const { type, value, html } = this.props;
+
+        return (
+            <li data-notification-type={type} className={css(styles[type])}>
+                {html ? (
+                    <div dangerouslySetInnerHTML={{ __html: html.__html }} />
+                ) : (
+                    <span>{value}</span>
+                )}
+            </li>
+        );
+    }
+}
+
+NotificationItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    html: PropTypes.shape({
+        __html: PropTypes.string,cd ..
+    }),
+};
+
+NotificationItem.defaultProps = {
+    type: 'default',
+    value: '',
+    html: null,
+};
+
+export default NotificationItem;
