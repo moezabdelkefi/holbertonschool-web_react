69a5eb5887058b41000652721140e47e4d6fdde7
moezabdelkef <moezbadelkefi17@gmail.com>
2024-03-10 10:51:50 +0100

    Combine store: modify the application

--- react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "./reducers/rootReducer";
 import App from "./App/App";
 import { loginRequest } from "./login";
 
